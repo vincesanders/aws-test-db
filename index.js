4c8a6fcc4cf6ebbfa93aa789e5800dc781c0079b
Vincent Sanders <vincesanders@hotmail.com>
2021-02-10 09:44:24 -0600

    Corrected path on server import.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const server = require('./api/server.js');
+const server = require('./server.js');
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
