75ef3783f754a68bba985073882956207520fb7b
Vincent Sanders <vincesanders@hotmail.com>
2021-02-10 10:05:47 -0600

    Hard coded production environment.

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,4 +3,4 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
 //switch to production when deploying
-module.exports = knex(knexConfig[process.env.DB_ENV]);
+module.exports = knex(knexConfig.production);
